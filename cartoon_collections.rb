04d91fa62db251cb6da004d28cc7e0881231a288
John Franti <john.franti@flatironschool.com>
2019-05-23 14:50:09 -0700

    add code to pass all tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, i|
+    puts "#{i+1}. #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  output = []
+  calls.map { |call|
+    call[0] = call[0].capitalize
+    output.push("#{call}!")
+  }
+  return output
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.each { |word|
+    if word.length > 4
+      return true
+    end
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each{|snack|
+    if cheese_types.include?(snack)
+      return snack
+    end
+  }
+  return nil
 end
